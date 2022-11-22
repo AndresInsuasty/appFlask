a4493a726a5a2082eefabfae07155bfdeaac4091
Andres Insuasty <andresinsuastyd10@gmail.com>
2022-11-21 21:59:28 -0500

    Estructura base desarrollo Listo

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+def pagina_no_encontrada(e):
+    return render_template('errores/404.html'), 404
+
+
+def inicializar_app(config):
+    app.config.from_object(config)
+    app.register_error_handler(404, pagina_no_encontrada)
+    return app
