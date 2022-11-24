50d159a822d1c5ad8ac4831dca8b1825a5f21135
AndresInsuasty <andresinsuastyd10@gmail.com>
2022-11-23 23:10:11 -0500

    trabajo con sesiones listo

--- app/__init__.py
+++ app/__init__.py
@@ -1,30 +1,34 @@
 from flask import Flask, render_template, request, url_for, redirect
 from flask_mysqldb import MySQL
 from flask_wtf.csrf import CSRFProtect
+from flask_login import LoginManager, login_user
 
 from .models.ModeloLibro import ModeloLibro
+from .models.ModeloUsuario import ModeloUsuario
+from .models.entities.Usuario import Usuario
 
 app = Flask(__name__)
 
 csrf = CSRFProtect()
 db = MySQL(app)
+login_manager_app = LoginManager(app)
+
+@login_manager_app.user_loader
+def load_user(id):
+    return ModeloUsuario.obtener_por_id(db, id)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    """
-    print(request.method)
-    print(request.form['username'])
-    print(request.form['password'])
-    return render_template('/auth/login.html')
-    """
     if request.method=='POST':
-        if  request.form['usuario'] == 'admin1' and request.form['password'] == '123456':
-        # print(request.form['usuario'])
-        # print(request.form['password'])
+        usuario=Usuario(None,request.form['usuario'],request.form['password'],None)
+        usuario_logeado = ModeloUsuario.login(db,usuario)
+        if  usuario_logeado != None:
+            login_user(usuario_logeado)
             return redirect(url_for('index'))
         else:
             return render_template('/auth/login.html')
---
+++ app/models/ModeloUsuario.py
@@ -0,0 +1,38 @@
+from .entities.Usuario import Usuario
+from .entities.TipoUsuario import TipoUsuario
+
+from werkzeug.security import check_password_hash
+
+
+class ModeloUsuario():
+    @classmethod
+    def login(self, db, usuario):
+        try:
+            cursor = db.connection.cursor()
+            sql = """SELECT id, usuario, password FROM usuario WHERE usuario = '{0}'""".format(
+                usuario.usuario)
+            cursor.execute(sql)
+            data = cursor.fetchone()
+            coincide = check_password_hash(data[2], usuario.password)
+            if coincide:
+                usuario_logeado = Usuario(data[0], data[1], None, None)
+                return usuario_logeado
+            else:
+                return None
+        except Exception as e:
+            raise Exception(e)
+
+    @classmethod
+    def obtener_por_id(self, db, id):
+        try:
+            cursor = db.connection.cursor()
+            sql = """SELECT USU.id, USU.usuario,TIP.id,TIP.nombre FROM usuario USU JOIN tipousuario TIP
+                    ON USU.tipousuario_id = TIP.id
+                    WHERE USU.id = {0}""".format(id)
+            cursor.execute(sql)
+            data = cursor.fetchone()
+            tipousuario = TipoUsuario(data[2], data[3])
+            usuario_logeado = Usuario(data[0], data[1], None, tipousuario)
+            return usuario_logeado
+        except Exception as e:
+            raise Exception(e)
